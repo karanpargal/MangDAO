05abd222594288d713b279614d3b370fe327abe8
Karan Pargal <karanpargal007@gmail.com>
2023-06-24 22:57:08 +0530

    Fix DAO card

--- client/src/Dashboard/DAOcards.js
+++ client/src/Dashboard/DAOcards.js
@@ -1,35 +1,36 @@
-import React from 'react'
-import { ArrowSmRightIcon } from '@heroicons/react/outline';
+import React from "react";
+import { ArrowSmRightIcon } from "@heroicons/react/outline";
 
 const DAOcards = (props) => {
-    return (
-        <div className=" " >
-          <div Name="DAO" className="grid grid-cols-1 lg:grid-cols-3  relative gap-x-8 gap-y-16 px-4 pt-12  sm:pt-20 text-black text-left ">
+  return (
+    <div className=" ">
+      <div
+        Name="DAO"
+        className="grid grid-cols-1 lg:grid-cols-3  relative gap-x-8 gap-y-16 px-4 pt-12  sm:pt-20 text-black text-left "
+      >
         <div className="bg-white rounded-xl shadow-2xl w-[400px] ">
           <img
             className="max-h-28 w-full rounded-lg mt-[-4rem]"
             src={props.DAOcards.Image}
+            alt="Logo of DAO"
           ></img>
-          <h3 className="font-bold text-2xl my-6 pl-8 " >
-          {props.DAOcards.Heading}</h3>
+          <h3 className="font-bold text-2xl my-6 pl-8 ">
+            {props.DAOcards.Heading}
+          </h3>
           <p className="text-gray-600 text-xl pl-8">
             {props.DAOcards.Description}
           </p>
 
           <div className="bg-slate-100 pl-8 py-4 mt-4">
             <button className="flex item-center text-indigo-600">
-              
+              {props.DAOcards.Button}
               <ArrowSmRightIcon className="w-5 ml-2" />
-              {props.DAOcards.button}
             </button>
           </div>
         </div>
-        </div>
-        </div>
-       
-      );
-    };
-    
+      </div>
+    </div>
+  );
+};
 
-
-export default DAOcards
+export default DAOcards;
--- client/src/Dashboard/DAOdetails.js
+++ client/src/Dashboard/DAOdetails.js
@@ -5,34 +5,34 @@ import DAOcards from "./DAOcards";
 const DAOdetails = () => {
   const DAOcardsData = [
     {
-      Image: "https://www.google.com/search?sxsrf=APwXEddBXl5owylZlc0z20u0_LHiWwdtdQ:1687544568841&q=superteam&tbm=isch&sa=X&ved=2ahUKEwjK3MW0gdr_AhWbd2wGHZppA0IQ0pQJegQIDRAB&biw=1536&bih=714&dpr=1.25#imgrc=2dnlMmnQwGuUUM",
+      Image: Superteam,
       Heading: "Superteam",
       Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
       Button: "Join",
     },
     {
-      Image: "https://www.google.com/search?sxsrf=APwXEddBXl5owylZlc0z20u0_LHiWwdtdQ:1687544568841&q=superteam&tbm=isch&sa=X&ved=2ahUKEwjK3MW0gdr_AhWbd2wGHZppA0IQ0pQJegQIDRAB&biw=1536&bih=714&dpr=1.25#imgrc=2dnlMmnQwGuUUM",
-      Heading: "Superteam",
-      Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
-      Button: "Join",
-    },
-
-    {
-      Image: "https://www.google.com/search?sxsrf=APwXEddBXl5owylZlc0z20u0_LHiWwdtdQ:1687544568841&q=superteam&tbm=isch&sa=X&ved=2ahUKEwjK3MW0gdr_AhWbd2wGHZppA0IQ0pQJegQIDRAB&biw=1536&bih=714&dpr=1.25#imgrc=2dnlMmnQwGuUUM",
+      Image: Superteam,
       Heading: "Superteam",
       Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
       Button: "Join",
     },
 
     {
-      Image: "https://www.google.com/search?sxsrf=APwXEddBXl5owylZlc0z20u0_LHiWwdtdQ:1687544568841&q=superteam&tbm=isch&sa=X&ved=2ahUKEwjK3MW0gdr_AhWbd2wGHZppA0IQ0pQJegQIDRAB&biw=1536&bih=714&dpr=1.25#imgrc=2dnlMmnQwGuUUM",
+      Image: Superteam,
       Heading: "Superteam",
       Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
       Button: "Join",
     },
 
     {
-      Image: { Superteam },
+      Image: Superteam,
+      Heading: "Superteam",
+      Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
+      Button: "Join",
+    },
+
+    {
+      Image:  Superteam,
       Heading: "Superteam",
       Description: "Lorem ipsum matilda elit da capsus whateva list gdreol ",
       Button: "Join",
@@ -42,10 +42,10 @@ const DAOdetails = () => {
   return (
     <div id="ListDetails">
       <div className="">
-        <DAOcards ListCards={DAOcardsData[0]} />
-        <DAOcards ListCards={DAOcardsData[1]} />
-        <DAOcards ListCards={DAOcardsData[2]} />
-        <DAOcards ListCards={DAOcardsData[3]} />
+        <DAOcards DAOcards={DAOcardsData[0]} />
+        <DAOcards DAOcards={DAOcardsData[1]} />
+        <DAOcards DAOcards={DAOcardsData[2]} />
+        <DAOcards DAOcards={DAOcardsData[3]} />
       </div>
     </div>
   );
