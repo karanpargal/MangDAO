546d1830d321a9ad3460d71cc4cfc0f5d856e834
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2023-06-24 10:15:32 +0530

    Added Dashboard Cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,19 @@
-import { Routes, Route } from "react-router-dom"
-import './App.css';
-import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
 import Landing from "./Components/Landing";
+import Dashboard from "./Dashboard/Dashboard";
+import DAOdetails from "./Dashboard/DAOdetails";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Landing/> } />
-        </Routes>
-     
+        <Route path="/" element={<Landing />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="DAOdetails" element={<DAOdetails />} />
+      
+      </Routes>
     </div>
   );
 }
