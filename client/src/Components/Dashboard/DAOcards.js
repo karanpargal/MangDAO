486fcf85ef99cfe90846a8abe11d5695a4fa4061
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2023-06-24 23:43:27 +0530

    Added DAOdetails

--- client/src/Components/Dashboard/DAOcards.js
+++ client/src/Components/Dashboard/DAOcards.js
@@ -7,7 +7,7 @@ const DAOcards = (props) => {
           <div Name="DAO" className="grid grid-cols-1 lg:grid-cols-3  relative gap-x-8 gap-y-16 px-4 pt-12  sm:pt-20 text-black text-left ">
         <div className="bg-white rounded-xl shadow-2xl w-[400px] ">
           <img
-            className="max-h-28 w-full rounded-lg mt-[-4rem]"
+            className="max-h-32 w-full rounded-xl mt-[-4rem]"
             src={props.DAOcards.Image}
           ></img>
           <h3 className="font-bold text-2xl my-6 pl-8 " >
@@ -16,9 +16,9 @@ const DAOcards = (props) => {
             {props.DAOcards.Description}
           </p>
 
-          <div className="flex bg-slate-100 pl-8 py-4 mt-4">
-          <ArrowSmRightIcon className="  w-5 ml-2  text-indigo-600" />
-            <button className=" item-center text-indigo-600 border-none">
+          <div className="flex bg-slate-100 pl-4 py-4 mt-4">
+          <ArrowSmRightIcon className=" w-5  text-indigo-600" />
+            <button className=" item-center text-indigo-600 ">
               
               
               {props.DAOcards.Button}
